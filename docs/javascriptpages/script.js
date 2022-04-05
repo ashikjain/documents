f675d617a3efa19801a51aa3deefa0d456b53819
ashikjain <ashikjain@users.noreply.github.com>
2022-04-05 12:48:50 +0530

    updating images

--- docs/javascriptpages/script.js
+++ docs/javascriptpages/script.js
@@ -1,8 +1,15 @@
-var img1=document.getElementById("srcimg");
+function enlargeimage(img2){
+    img2.style.width="50%" ;
+    img2.style.height="200%" ;
+    img2.style.transform = "scale(1.5)";
+    img2.style.margin="70px";
+    img2.style.transition ="transform 2s ease";
+}
 
-img1.onclick=function(){
-    img1.src=this.src;
-    img1.width=100 ;
-    img1.height=100 ;
-
+function resize(srcimg){
+    srcimg.style.width="auto" ;
+    srcimg.style.height="auto" ;
+    srcimg.style.transform = "scale(1.5)";
+    srcimg.style.transition ="transform 0.2s ease";
+    srcimg.style.margin="40px";
 }
